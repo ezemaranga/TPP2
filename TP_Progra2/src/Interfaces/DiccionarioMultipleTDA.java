8d21727002b8b31064cd0d5f3ef8124b3fe682d2
uade <uade@702PC04.labs>
2015-11-03 21:29:20 -0300

    agregando otras cosas (puto)

---
+++ TP_Progra2/src/Interfaces/DiccionarioMultipleTDA.java
@@ -0,0 +1,12 @@
+package Interfaces;
+
+import TDA.ConjuntoTDA;
+
+public interface DiccionarioMultipleTDA {
+	void InicializarDiccionario(); // PC: -
+	void Agregar(int clave, int valor); // PC: Diccionario Inicializado
+	void Eliminar(int clave); // PC: Diccionario Inicializado
+	void EliminarValor(int clave, int valor); // PC: Diccionario Inicializado
+	ConjuntoTDA Recuperar(int clave); // PC: Diccionario Inicializado
+	ConjuntoTDA Claves(); // PC: Diccionario Inicializado
+}
